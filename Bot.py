571c0a1d28b699dc1709ea332cab2502c3a19050
antonynaumovic <40827853+antonynaumovic@users.noreply.github.com>
2019-06-04 20:26:11 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -205,7 +205,7 @@ async def nuke(ctx):
     await bot.say("1")
     await asyncio.sleep(1)
     await bot.say("0")
-    for i in ctx.message.server.channel:
+    for i in ctx.message.server.channels:
         x=x+1
         await bot.say("nuking {}").format(str(i))
     await bot.say("{} Channels Have Been Nuked").format(x)
